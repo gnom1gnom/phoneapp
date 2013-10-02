7f6de262aea53b40e20394969273713fd4bcba0a
gnom1gnom <gnom1gnom@yahoo.com>
2013-10-02 17:31:45 +0200

    poprawka testu z tagami

--- test/e2e/view-tags.js
+++ test/e2e/view-tags.js
@@ -3,25 +3,18 @@
 describe('my app', function() {
 	// tablica opcji selecta wygenerowanego dla facetu tags
 	var tagsList = [
-		'Tag foo',
-		'Tag bar',
-		'Tag baz',
-		'Tag blach',
-		'Tag fnord'
+		['Tag foo'],
+		['Tag bar'],
+		['Tag baz'],
+		['Tag blach'],
+		['Tag fnord']
 	];
 
 	it('should list all tags', function() {
 		browser().navigateTo('/#/tags');
-		var foundTags = element('#tags li').query(function(elements, done) {
-			var tagsArray = [];
-			// sprawdzamy poszczególne opcje
-			elements.each(function(index) {
-				tagsArray.push(elements[index].innerText);
-			});
-
-			done(null, tagsArray);
-		});
-
-		expect(foundTags).toEqual(tagsList);
+		expect(repeater('#tags li').count()).toEqual(tagsList.length);
+		for (var i = 0; i < tagsList.length; i++) {
+			expect(repeater('#tags li').row(i)).toEqual(tagsList[i]);
+		}
 	});
 });
